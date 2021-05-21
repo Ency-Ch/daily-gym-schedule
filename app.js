9bb7febe29f4fcd8ec284fd5a019b4fa0a9c94c4
ency ch <encee07@yahoo.co.uk>
2021-05-21 09:50:23 +0100

    changes to the UI, week to day; local storage set up

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-var       date = document.getElementById("date").value,
+/*8*let  date = document.getElementById("date").value,
           body_part = document.getElementById("body-area"),
           selected_body_part = getSelectedOption(body_part).text,
           exercise = document.getElementById("exercise").value 
-
+*/
 var opt;
 
 function getSelectedOption(sel) {
@@ -16,6 +16,7 @@ function getSelectedOption(sel) {
     }
     return opt;
 }
+
 
 
 
@@ -41,7 +42,7 @@ class UI {
         row.innerHTML = `<td>${gymschedule.date}</td>
                          <td>${gymschedule.selected_body_part}</td>
                          <td>${gymschedule.exercise}</td>
-                         <td><a href ='#' class = delete></a>X</td>`
+                         <td><a href ='#' class = delete>X</a></td>`
 
                          list.appendChild(row)
 }
@@ -49,7 +50,7 @@ class UI {
 
 showAlert(message, className){
     //create a div 
-    var div = document.createElement('div');
+    const div = document.createElement('div');
 
     //add class to div
 
@@ -59,9 +60,9 @@ showAlert(message, className){
     div.appendChild(document.createTextNode(message));
 
     //get parent 
-    var container = document.querySelector('.container');
+    const container = document.querySelector('.container');
     //get form 
-    var form = document.querySelector('#book-form');
+    const form = document.querySelector('#book-form');
     //insert alert 
     container.insertBefore(div, form)
 
@@ -72,44 +73,123 @@ showAlert(message, className){
     }, 3000)
 
 }
+clearFields(){
+
+    document.getElementById("date").value ='';
+    document.getElementById("body-area").value = '';
+    //selected_body_part = getSelectedOption(body_part).text
+    document.getElementById("exercise").value = '';
+   }
+
+deleteSchedule(target){
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove()
+    }
 
 }
 
 
+}
 
+class Store {
+    static getSchedules() {
+      let gymschedules;
+      if(localStorage.getItem('gymschedules') === null) {
+        gymschedules = [];
+      } else {
+        gymschedules = JSON.parse(localStorage.getItem('gymschedules'));
+      }
+  
+      return gymschedules;
+    }
+  
+    static displaySchedule() {
+      const gymschedules = Store.getSchedules();
+  
+      gymschedules.forEach(function(gymschedule){
+        const ui  = new UI;
+  
+        // Add book to UI
+        ui.addScheduleToList (gymschedule)
+      });
+    }
+  
+    static addSchedule(gymschedule) {
+      const gymschedules = Store.getSchedules();
+  
+      gymschedules.push(gymschedule);
+  
+      localStorage.setItem('gymschedules', JSON.stringify(gymschedules));
+    }
+    static removeSchedule(exercise) {
+        const gymschedules = Store.getSchedules();
+    
+        gymschedules.forEach(function(gymschedule, index){
+         if(gymschedule.exercise === exercise) {
+          gymschedules.splice(index, 1);
+         }
+        });
+    
+        localStorage.setItem('gymschedules', JSON.stringify(gymschedules));
+      }
+  }
 document.getElementById('book-form').addEventListener('submit', function(e){
     //get values
-    var date = document.getElementById("date").value,
+    const date = document.getElementById("date").value,
     body_part = document.getElementById("body-area"),
     selected_body_part = getSelectedOption(body_part).text
-   
     exercise = document.getElementById("exercise").value
 
-     if (date != '' && selected_body_part != '---select---' && exercise != '' ) {
-        // instatiate a new schedule 
-        const gymschedule = new GymSchedule(date, selected_body_part,exercise);
+   
 
-        //instatiate a new UI 
-        const ui = new UI();
-        ui.addScheduleToList(gymschedule)
-        ui.showAlert('schedule added', 'success')
+
+     if (date != '' && selected_body_part != '---select---' && exercise != '' ) {
+     // instatiate a new schedule 
+    const gymschedule = new GymSchedule(date, selected_body_part,exercise);
+    //instatiate a new UI 
+    const ui = new UI();
+        
+        ui.addScheduleToList(gymschedule);
+
+         //add to local storage
+        Store.addSchedule(gymschedule)
+       
+       
+        ui.showAlert('schedule added', 'success');
+        ui.clearFields();
+
+       
 
      } else{
-         const ui = new UI()
-         ui.showAlert('please fill all fields', 'error')
-     }   
- 
+        const ui = new UI();
+        
+         ui.showAlert('please fill all fields', 'error');
+     }  
 
+  
   
 
   e.preventDefault();
 
 })
+//DOM Load event for display 
+document.addEventListener('DOMContentLoaded', Store.displaySchedule)
 
 
 
+//event listner for delete
+document.getElementById('gym-list').addEventListener('click', function(e){
+    //insatiate a new ui object 
+    const ui = new UI()
+    ui.deleteSchedule(e.target)
+    Store.removeSchedule(e.target.parentElement.previousElementSibling.textContent)
+    console.log(Store.removeSchedule(e.target.parentElement.previousElementSibling.textContent))
+    ui.showAlert('schedule removed', 'success')
+    
+
+    e.preventDefault()
+    console.log(e.target)
+})
 
 
-
-
-
+/// event listner fo the delete and remove from storage 
